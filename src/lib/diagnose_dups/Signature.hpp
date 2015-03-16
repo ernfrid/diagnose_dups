907bc2c1b03c0375bc959ee074beca2511ea99bb
Dave Larson <dlarson@genome.wustl.edu>
2015-03-16 16:33:12 -0500

    basic classes to do signature creation. untested, but compile

---
+++ src/lib/diagnose_dups/Signature.hpp
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <sam.h>
+#include <boost/functional/hash.hpp>
+
+#include <stdint.h>
+
+//the intent is for this class to store samblaster signatures
+//they will be used as hash keys in an unordered map
+//they will be created directly from a bam1_t record
+
+class Signature {
+    public:
+        int32_t tid;
+        int32_t mtid;
+        int32_t pos;
+        int32_t mpos;
+        bool reverse;
+        bool mreverse;
+
+    Signature()
+        : tid(-1)
+        , mtid(-1)
+        , pos(-1)
+        , mpos(-1)
+        , reverse(false)
+        , mreverse(false)
+    {}
+
+    Signature(bam1_t const* record)
+        : tid(record->core.tid)
+          , mtid(record->core.mtid)
+          , pos(_calculate_position(record))
+          , mpos(_calculate_mate_position(record))
+          , reverse(bam_is_rev(record))
+          , mreverse(bam_is_mrev(record))
+    {}
+
+    bool operator==(Signature const& rhs) const {
+        return tid == rhs.tid
+            && mtid == rhs.mtid
+            && pos == rhs.pos
+            && mpos == rhs.mpos
+            && reverse == rhs.reverse
+            && mreverse == rhs.mreverse;
+    }
+
+    friend std::size_t hash_value(Signature const& sig) {
+        std::size_t seed = 0;
+        boost::hash_combine(seed, sig.tid);
+        boost::hash_combine(seed, sig.mtid);
+        boost::hash_combine(seed, sig.pos);
+        boost::hash_combine(seed, sig.mpos);
+        boost::hash_combine(seed, sig.reverse);
+        boost::hash_combine(seed, sig.mreverse);
+        return seed;
+    }
+
+    private:
+    int32_t _calculate_position(bam1_t const* record);
+    int32_t _calculate_mate_position(bam1_t const* record);
+
+    int32_t _calculate_right_offset(bam1_t const* record);
+    int32_t _calculate_right_offset(char const* cigar);
+    int32_t _calculate_left_offset(bam1_t const* record);
+    int32_t _calculate_left_offset(char const* cigar);
+};
