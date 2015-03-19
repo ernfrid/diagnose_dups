769c38933f8e2f663340c414e0ec651378e64987
Dave Larson <dlarson@genome.wustl.edu>
2015-03-18 21:50:38 -0500

    some code to make things run. Should be extracted into a class

--- src/exe/diagnose-dups/diagnose_dups.cpp
+++ src/exe/diagnose-dups/diagnose_dups.cpp
@@ -36,9 +36,98 @@ diagnose_dups -i bam_file -o dup_stats
 */
 
 #include "common/Options.hpp"
+#include "diagnose_dups/Read.hpp"
+#include "diagnose_dups/Signature.hpp"
+
+#include <boost/unordered_map.hpp>
+
+#include <sam.h>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char** argv) {
     Options opts = Options(argc, argv);
 
+    htsFile *fp = hts_open(opts.vm["input"].as<string>().c_str(), "r");
+    if(fp == 0) {
+       cerr << "Unable to open " << opts.vm["input"].as<string>() << "\n";
+       exit(1);
+    }
+    bam_hdr_t *header = sam_hdr_read(fp);
+    bam1_t *record = bam_init1();
+    
+    typedef vector<Read> read_vector;
+    typedef boost::unordered_map<Signature, read_vector> signature_map;
+    signature_map signatures;
+
+    int32_t last_pos = -1;
+    int32_t last_tid = -1;
+    int32_t pos_window = 300;
+    
+    typedef boost::unordered_map<int, int> histogram;
+    histogram dup_insert_sizes;
+    histogram distances;
+    histogram number_of_dups;
+
+    while(sam_read1(fp, header, record) >= 0) {
+        Read read(record);
+        Signature sig(record);
+        if(last_pos > -1) {
+            //grab iterators
+            //iterate over sigs in signatures
+            for(signature_map::iterator i = signatures.begin(); i != signatures.end(); ++i) {
+
+                if(last_tid != sig.tid 
+                        || (last_tid == i->first.tid && (last_pos - pos_window) > i->first.pos)) {
+                    if(i->second.size() < 2) {
+                        //remove, it's not a dup
+                        signatures.erase(i);
+                    }
+                    else {
+                        //it's a dup
+
+                        //add up number of dups
+                        number_of_dups[i->second.size()] += 1;
+
+                        typedef vector<Read>::iterator read_vec_iter;
+                        for(read_vec_iter current_read_iter = i->second.begin(); current_read_iter != i->second.end() - 1; ++current_read_iter) {
+                            dup_insert_sizes[abs(current_read_iter->insert_size)] += 1;
+                            for(read_vec_iter distance_calc_iter = current_read_iter + 1; distance_calc_iter != i->second.end(); ++distance_calc_iter) {
+                                if(current_read_iter->is_on_same_tile(*distance_calc_iter) &&
+                                        !(current_read_iter->flowcell == distance_calc_iter->flowcell
+                                         && current_read_iter->tile == distance_calc_iter->tile
+                                         && current_read_iter->lane == distance_calc_iter->lane
+                                         && current_read_iter->x == distance_calc_iter->x
+                                         && current_read_iter->y == distance_calc_iter->y)) {
+                                    int flow_cell_distance = current_read_iter->distance(*distance_calc_iter);
+                                    distances[flow_cell_distance] += 1;
+                                }
+                            }
+                        }
+
+
+
+
+                    }
+                }
+//                cout << "There are " << i->second.size() << " reads in this bin\n";
+            }
+        }
+        signatures[sig].push_back(read);
+        last_tid = sig.tid;
+        last_pos = sig.pos;
+        //cout << "The size of the buffer is " << signatures.size() << "\n";
+        //cout << "There are " << signatures.count(sig) << " buckets with current signature\n";
+        //cout << "There are " << signatures[sig].size() << " reads with current signature\n";
+    }
+    bam_destroy1(record);
+    bam_hdr_destroy(header);
+    hts_close(fp);
+
+    cout << "Inter-tile distance\tFrequency\n";
+    for(histogram::iterator i = distances.begin(); i != distances.end(); ++i) {
+        cout << i->first << "\t" << i->second << "\n";
+    }
     return 0;
 }
