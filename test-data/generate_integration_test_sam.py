1032b5dc2d3b1f7feb612d1b4fc08c3bc648dc31
Dave Larson <dlarson@genome.wustl.edu>
2015-03-23 23:10:30 -0500

    insert a read that messes with current bundling code

--- test-data/generate_integration_test_sam.py
+++ test-data/generate_integration_test_sam.py
@@ -7,6 +7,8 @@ header = """@HD	VN:1.3	SO:coordinate
 exemplar_read = """machine:1:flowcell:1:1101:1:1	99	1	15729	60	5M	=	16153	575	CAGGG	AAFFF	MC:Z:5M"""
 outside_tile_dup = """machine:1:flowcell:1:1102:1:1	99	1	15729	60	5M	=	16153	575	CAGGG	AAFFF	MC:Z:5M"""
 inside_tile_dup = """machine:1:flowcell:1:1101:1:10	99	1	15729	60	5M	=	16153	575	CAGGG	AAFFF	MC:Z:5M""" # distance should be 9
+intervening_read = """machine:1:flowcell:1:1102:1:20	99	1	15730	60	4M	=	16153	575	AGGG	AFFF	MC:Z:5M"""
+ 
 clipped_dup = """machine:1:flowcell:1:1102:1:13	99	1	15731	60	2S3M	=	16153	575	CAGGG	AAFFF	MC:Z:5M""" # distance should be 12
 
 non_dup = """machine:1:flowcell:2:1101:1:1	99	1	15750	60	5M	=	16153	375	GATCT	AAFFF	MC:Z:5M"""
@@ -25,6 +27,7 @@ print "\n".join((
     exemplar_read,
     outside_tile_dup,
     inside_tile_dup,
+    intervening_read,
     clipped_dup,
     non_dup,
     non_dup2,
