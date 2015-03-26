bca8a094b3dde40ed1687ed54f1bcff78f05386d
Dave Larson <dlarson@genome.wustl.edu>
2015-03-25 23:31:41 -0500

    some basic toplevel metrics

--- src/lib/diagnose_dups/BufferProcessor.hpp
+++ src/lib/diagnose_dups/BufferProcessor.hpp
@@ -15,9 +15,13 @@ struct BufferProcessor {
     Histogram<uint64_t> distances;
     Histogram<uint64_t> number_of_dups;
     std::size_t total_dups;
+    std::size_t total_fragments;
+    std::size_t total_intertile;
 
     BufferProcessor()
         : total_dups(0)
+        , total_fragments(0)
+        , total_intertile(0)
     {}
 
     void update_distances(ReadVector const& reads) {
@@ -30,13 +34,17 @@ struct BufferProcessor {
                     uint64_t flow_cell_distance = euclidean_distance(reads[i], reads[j]);
                     ++distances[flow_cell_distance];
                 }
+                else {
+                    ++total_intertile;
+                }
             }
         }
     }
     
     void operator()(ReadVector const& reads) {
+        total_fragments += reads.size();
         if (reads.size() > 1) {
-            ++total_dups;
+            total_dups += reads.size();
             ++number_of_dups[reads.size()];
             update_distances(reads);
         }
@@ -57,10 +65,18 @@ struct BufferProcessor {
     //XXX This is terrible and should abstracted out
     void write_output(std::ostream& os) {
         os << "{\n"; //start json
+        os << " \"summary\": [\n";
+        os << "    { "
+           << "\"total_fragments\": " << total_fragments
+           << ", "
+           << "\"total_duplicate_fragments\": " << total_dups
+           << ", "
+           << "\"total_intertile_duplicates\": " << total_intertile
+           << " }\n ],\n";
         typedef Histogram<uint64_t>::VectorType HVec;
         HVec dist = distances.as_sorted_vector();
         os << " \"distance\": [\n";
-        for(HVec::const_iterator i = dist.begin(); i != dist.end(); ++i) {
+        for (HVec::const_iterator i = dist.begin(); i != dist.end(); ++i) {
             os << "    { "
                << "\"intertile_distance\": " << i->name
                << ", "
@@ -74,7 +90,7 @@ struct BufferProcessor {
 
         os << " \"num_times_duplicated\": [\n";
         HVec ndup = number_of_dups.as_sorted_vector();
-        for(HVec::const_iterator i = ndup.begin(); i != ndup.end(); ++i) {
+        for (HVec::const_iterator i = ndup.begin(); i != ndup.end(); ++i) {
             os << "    { "
                << "\"num_duplicates\": " << i->name
                << ", "
@@ -88,7 +104,7 @@ struct BufferProcessor {
 
         os << " \"insert_size\": [\n";
         HVec isizes = nondup_insert_sizes.as_sorted_vector();
-        for(HVec::const_iterator i = isizes.begin(); i != isizes.end(); ++i) {
+        for (HVec::const_iterator i = isizes.begin(); i != isizes.end(); ++i) {
             os << "    { "
                << "\"insert_size\": " << i->name
                << ", "
@@ -102,6 +118,6 @@ struct BufferProcessor {
         }
         os << "\n ]\n}\n";
 
-        std::cerr << total_dups << " duplicates found.\n";
+        std::cerr << total_dups << " duplicates found out of " << total_fragments << ".\n";
     }
 };
