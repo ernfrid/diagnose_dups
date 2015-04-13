20b7eaeffda2b4aa84755bf83a6d2be22ffdfcb6
Dave Larson <dlarson@genome.wustl.edu>
2015-04-13 16:18:40 -0500

    finish adding tests for Tile struct

--- test/lib/diagnose_dups/TestTile.cpp
+++ test/lib/diagnose_dups/TestTile.cpp
@@ -35,3 +35,35 @@ TEST_F(TestTile, same_tile) {
     ASSERT_FALSE(same_tile(test_tile, test_tile2));
 }
     
+TEST_F(TestTile, hash_value) {
+    Tile test_tile;
+    test_tile.flowcell = "a";
+    test_tile.lane = 1;
+    test_tile.id = 1101;
+    test_tile.subtile_x = 1;
+    test_tile.subtile_y = 5;
+
+    Tile test_tile2 = test_tile;
+
+    ASSERT_TRUE(hash_value(test_tile) == hash_value(test_tile2));
+
+    test_tile2.subtile_y = 6;
+    ASSERT_FALSE(hash_value(test_tile) == hash_value(test_tile2));
+}
+
+TEST_F(TestTile, less) {
+    Tile test_tile;
+    test_tile.flowcell = "a";
+    test_tile.lane = 1;
+    test_tile.id = 1101;
+    test_tile.subtile_x = 1;
+    test_tile.subtile_y = 5;
+
+    Tile test_tile2 = test_tile;
+
+    ASSERT_FALSE(test_tile < test_tile2);
+
+    test_tile2.subtile_y = 6;
+    ASSERT_TRUE(test_tile < test_tile2);
+
+}
