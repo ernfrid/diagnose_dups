278dc202f7527461171a64514632bce091a0b69d
Dave Larson <dlarson@genome.wustl.edu>
2015-09-09 00:01:03 -0500

    attempt to switch over to packaged Boost

--- cmake/BuildBoost.cmake
+++ cmake/BuildBoost.cmake
@@ -1,8 +1,6 @@
 cmake_minimum_required(VERSION 2.8)
 
-include(ExternalProject)
-
-set(DEFAULT_BOOST_URL ${CMAKE_SOURCE_DIR}/vendor/boost-1.54-breakdancer.tar.gz)
+set(DEFAULT_BOOST_URL ${CMAKE_SOURCE_DIR}/vendor/boost_1_59_0.diagnose_dups.tgz)
 if(NOT DEFINED BOOST_URL)
     set(BOOST_URL ${DEFAULT_BOOST_URL})
 endif(NOT DEFINED BOOST_URL)
@@ -24,17 +22,19 @@ endforeach(libname ${REQUIRED_BOOST_LIBS})
 message("Extracting boost from ${BOOST_URL}")
 message("Boost build log can be found at ${BOOST_LOG}")
 
-ExternalProject_Add(
-    boost-1.54
-    URL ${BOOST_URL}
-    SOURCE_DIR ${BOOST_SRC}
-    BINARY_DIR ${BOOST_SRC}
-    CONFIGURE_COMMAND "./bootstrap.sh"
-    BUILD_COMMAND
-        echo "Building boost, build log is ${BOOST_LOG}" &&
-        ./b2 --prefix=${BOOST_ROOT} --layout=system link=static
-                threading=multi install ${BOOST_BUILD_LIBS} > ${BOOST_LOG} 2>&1
-    INSTALL_COMMAND ""
+ExternalDependency_Add(
+    boost-1.59
+    BUILD_BYPRODUCTS ${Boost_LIBRARIES}
+    ARGS
+        URL ${BOOST_URL}
+        SOURCE_DIR ${BOOST_SRC}
+        BINARY_DIR ${BOOST_SRC}
+        CONFIGURE_COMMAND "./bootstrap.sh"
+        BUILD_COMMAND
+            echo "Building boost, build log is ${BOOST_LOG}" &&
+            ./b2 --prefix=${BOOST_ROOT} --layout=system link=static
+                    threading=multi install ${BOOST_BUILD_LIBS} > ${BOOST_LOG} 2>&1
+        INSTALL_COMMAND "true"
 )
 
 set(Boost_INCLUDE_DIRS ${BOOST_ROOT}/include)
